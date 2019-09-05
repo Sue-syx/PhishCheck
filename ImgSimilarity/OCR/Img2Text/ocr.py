3930a74d95f206755c49e751181ab71b72ee202e
Sue-syx <54345858+Sue-syx@users.noreply.github.com>
2019-09-05 10:24:04 +0800

    Add files via upload

--- ImgSimilarity/OCR/Img2Text/ocr.py
+++ ImgSimilarity/OCR/Img2Text/ocr.py
@@ -1,1 +1,8 @@
-ss
+from PIL import Image
+import pytesseract
+import cv2 as cv
+
+img_path = 'images/netflex.png'
+img = cv.imread(img_path)
+text = pytesseract.image_to_string(Image.fromarray(img))
+print(text)
---
+++ ImgSimilarity/OCR/Img2Text/ocrFillter.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import random
+import cv2 as cv
+from PIL import Image
+import pytesseract
+
+
+def dataWash(path):
+    for root, dirs, files in os.walk(path):
+        count = 0
+        for dir_ in dirs:
+            print(str(dir_))
+            count+=1
+            # if count<18000:
+            #     continue
+            if count>2000:
+                break
+            imgpath = os.path.join(root, dir_) + "/shot.png"
+            htmlpath = os.path.join(root, dir_) + "/html.txt"
+            if os.path.exists(htmlpath) and os.path.exists(imgpath):
+                with open(htmlpath) as file_object:
+                    content = file_object.read()
+                flag = ((os.path.getsize(imgpath) / 1024 > 10)
+                        and (not "NUS" in content)
+                        and (not "domain" in content)
+                        and (not "sorry" in content)
+                        and (not "Suspended" in content)
+                        and (not "The website you were trying to reach is temporarily unavailable" in content))
+                if flag:
+                    img = cv.imread(imgpath)
+                    text = pytesseract.image_to_string(Image.fromarray(img))
+                    brandname = dir_[:dir_.find("+")]
+                    brand = brandname.split()
+                    for brsplit in brand:
+                        if brsplit.lower() in text.lower():
+                            oldpath = "D:\\yuxuan\\phishwebsGet\\Database\\" + str(dir_)
+                            newpath = "D:\\yuxuan\\phishwebsGet\\newDatabase\\" + str(dir_)
+                            try:
+                                shutil.copytree(oldpath, newpath)
+                                break
+                            except Exception as e:
+                                print(e)
+
+
+if __name__ == '__main__':
+    path = "D:\\yuxuan\\phishwebsGet\\Database"
+    dataWash(path)
